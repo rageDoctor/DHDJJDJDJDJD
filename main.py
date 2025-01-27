c76fb223557bfd6c8d1233e13c3f15e7c07c289a
rageDoctor <xxfdd365@gmail.com>
2025-01-27 17:06:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -175,7 +175,7 @@ def usernameG():
          u10 = k + k + v + n + k
          u11 = k + v + k + n + k
          s = u1, u2, u3, u4, u5, u6, u7, u8, u9, u10, u11
-        return choice(s)
+         return choice(s)
     elif type == "f5":
         u1 = k + n + c + bot
         u2 = k + n + c + bot
